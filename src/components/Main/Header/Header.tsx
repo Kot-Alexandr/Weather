4345af0103dfd4b8524714089d8ae31d13811510
Kot-Alexandr <voollkot@gmail.com>
2023-09-05 21:39:37 +0300

    Create style for Header

--- src/components/Main/Header/Header.tsx
+++ src/components/Main/Header/Header.tsx
@@ -1,4 +1,5 @@
 import s from './Header.module.scss'
+import sunSVG from './../../../accets/img/sun.svg'
 
 const Header = () => {
     return (
@@ -6,7 +7,10 @@ const Header = () => {
             <div className={s.HeaderTextBlock}>
                 <div className={s.HeaderTextTime}>07:32 AM</div>
                 <div className={s.HeaderTextDate}>Wednesday, 14 April, 2023</div>
-                <div className={s.HeaderTextDayTime}>Good morning My wise friend</div>
+                <div className={s.HeaderTextImgAndTextDay}>
+                    <img src={sunSVG} className={s.HeaderTextImg} />
+                    <div className={s.HeaderTextDayTime}>Good morning My wise friend!</div>
+                </div>
             </div>
             <div>
                 {/* <Switch/> тут будет с материал юай */}
